f7beb2ed64ca210d81c2f3f2d0f5fbf3547e562c
rohitraj1104 <37894315+rohitraj1104@users.noreply.github.com>
2021-01-26 20:54:59 +0530

    Dice Game

---
+++ RandomNumber.py
@@ -0,0 +1,8 @@
+# if you dont have dice to play game then this code will help you out
+
+import random
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+print("DICE1 :",dice1)
+print("==============")
+print("DICE2 :" ,dice2)
